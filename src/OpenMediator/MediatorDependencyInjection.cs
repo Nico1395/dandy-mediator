0ecd6b35e8c125b18dc778907fad43b22fe4c937
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-04-29 11:30:45 +0200

    Added implementation and di

---
+++ src/OpenMediator/MediatorDependencyInjection.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.DependencyInjection;
+using OpenMediator.Commands;
+using OpenMediator.Middleware;
+using OpenMediator.Queries;
+using System.Reflection;
+
+namespace OpenMediator;
+
+public static class MediatorDependencyInjection
+{
+    private static readonly IReadOnlyList<Type> _requestHandlerInterfaceTypes =
+    [
+        typeof(IRequestHandler<>),
+        typeof(IRequestExceptionHandler<>),
+        typeof(IRequestMiddleware<>),
+        typeof(IRequestHandler<,>),
+        typeof(IRequestExceptionHandler<,>),
+        typeof(IRequestMiddleware<,>),
+        typeof(IGlobalRequestExceptionHandler),
+        typeof(IQueryHandler<,>),
+        typeof(ICommandHandler<>),
+        typeof(ICommandHandler<,>),
+        typeof(INotificationHandler<>),
+        typeof(INotificationExceptionHandler<>),
+        
+    ];
+
+    public static IServiceCollection AddOpenMediator(this IServiceCollection services, IReadOnlyList<Assembly> assemblies)
+    {
+        services.AddTransient<IMediator, Mediator>();
+        services.AddTransient(typeof(IRequestMiddleware<,>), typeof(ResponseRequestValidationMiddleware<,>));
+
+        AddRequestHandlersFromAssemblies(services, assemblies);
+
+        return services;
+    }
+
+    private static void AddRequestHandlersFromAssemblies(IServiceCollection services, IReadOnlyList<Assembly> assemblies)
+    {
+        var handlerTypes = assemblies.SelectMany(a => a.DefinedTypes).Where(t => t.IsClass && !t.IsAbstract && !t.IsGenericTypeDefinition);
+        foreach (var implementationType in handlerTypes)
+        {
+            var interfaces = implementationType.ImplementedInterfaces;
+            foreach (var @interface in interfaces)
+            {
+                if (!@interface.IsGenericType)
+                    continue;
+
+                var genericDefinition = @interface.GetGenericTypeDefinition();
+                if (_requestHandlerInterfaceTypes.Contains(genericDefinition))
+                    services.AddTransient(@interface, implementationType);
+            }
+        }
+    }
+}
