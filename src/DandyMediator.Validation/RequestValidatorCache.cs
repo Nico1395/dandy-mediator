47a24982ef3dc68a154808d05d2a3c7b23132edb
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-07-19 20:20:55 +0200

    removed constructors from checking whether a request should be validated

--- src/DandyMediator.Validation/RequestValidatorCache.cs
+++ src/DandyMediator.Validation/RequestValidatorCache.cs
@@ -12,20 +12,11 @@ internal static class RequestValidatorCache
     {
         return _cache.GetOrAdd(type, CreateMetadata);
     }
-    
+
     private static RequestValidationMetadata CreateMetadata(Type type)
     {
-        // Properties
         if (type.GetProperties().Any(p => p.GetCustomAttributes<ValidationAttribute>(true).Any()))
             return new RequestValidationMetadata(true);
-
-        // Constructor parameter attributes for records
-        var constructors = type.GetConstructors();
-        foreach (var constructor in constructors)
-        {
-            if (constructor.GetParameters().Any(p => p.GetCustomAttributes<ValidationAttribute>(true).Any()))
-                return new RequestValidationMetadata(true);
-        }
 
         return new RequestValidationMetadata(false);
     }
