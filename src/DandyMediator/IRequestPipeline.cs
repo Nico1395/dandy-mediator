282384149d845b7d857bb3ae214d9e329e2d1ef9
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-08-25 22:24:05 +0200

    added test project for the core package

--- src/DandyMediator/IRequestPipeline.cs
+++ src/DandyMediator/IRequestPipeline.cs
@@ -1,6 +1,6 @@
 namespace DandyMediator;
 
-public interface IRequestPipeline
+internal interface IRequestPipeline
 {
     Task ExecuteAsync<TRequest>(TRequest request, CancellationToken cancellationToken) where TRequest : IRequest;
     Task<TResponse> ExecuteAsync<TRequest, TResponse>(TRequest request, CancellationToken cancellationToken) where TRequest : IRequest<TResponse>;
